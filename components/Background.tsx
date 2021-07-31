63df06917a918a2fdb094f5910feb9d7ab50f790
Takashi IIGUNI <iiguni.tks@gmail.com>
2021-08-01 00:28:34 +0900

    fix build error

--- components/Background.tsx
+++ components/Background.tsx
@@ -2,14 +2,22 @@ import { Event } from "../lib/Events";
 import Career from './Career';
 
 type Props = {
-  events: [Event]
+  events: Event[]
 }
 
-const Background = ({ events }: [Event]) => (
+const Background = ({ events }: Props ) => (
   <div>
     <h2>Backgroud</h2>
     <ul>
-      { events.map((event) => <li><Career date={event.date} body={event.body} /></li>) }
+      {
+        events.map((event) =>
+        <li key={event.id}>
+          <Career
+            id={event.id}
+            date={event.date}
+            body={event.body} />
+        </li>)
+      }
     </ul>
   </div>
 )
--- lib/Events.ts
+++ lib/Events.ts
@@ -1,13 +1,14 @@
 export type Event = {
-  date?: Date,
-  body?: string
+  id: number
+  date: Date,
+  body: string
 }
 
-export const events: [Event] = [
-  { date: new Date('2013-04'), body: "島根県立出雲工業高等学校電気科 入学"},
-  { date: new Date('2016-03'), body: "島根県立出雲工業高等学校電気科 卒業"},
-  { date: new Date('2016-04'), body: "香川大学工学部 入学"},
-  { date: new Date('2020-03'), body: "香川大学工学部 卒業"},
-  { date: new Date('2020-04'), body: "香川大学大学院工学研究科 入学"},
-  { date: new Date(),          body: "香川大学大学院工学研究科 在学中"},
+export const events: Event[]= [
+  { id: 1, date: new Date('2013-04'), body: "島根県立出雲工業高等学校電気科 入学"},
+  { id: 2, date: new Date('2016-03'), body: "島根県立出雲工業高等学校電気科 卒業"},
+  { id: 3, date: new Date('2016-04'), body: "香川大学工学部 入学"},
+  { id: 4, date: new Date('2020-03'), body: "香川大学工学部 卒業"},
+  { id: 5, date: new Date('2020-04'), body: "香川大学大学院工学研究科 入学"},
+  { id: 6, date: new Date(),          body: "香川大学大学院工学研究科 在学中"},
 ]
