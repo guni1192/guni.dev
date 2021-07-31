4b06d8770bed2c7d52dea5729329db77da249278
Takashi IIGUNI <iiguni.tks@gmail.com>
2021-08-01 02:01:21 +0900

    fix: notation is off by a month in Background

--- components/Career.tsx
+++ components/Career.tsx
@@ -2,7 +2,7 @@ import { Event } from '../lib/Events'
 
 const Career = ({ date, body }: Event) => (
   <div>
-    {date.getFullYear()}年{date.getMonth()}月: { body }
+    {date.getFullYear()}年{date.getMonth() + 1}月: { body }
   </div>
 )
 
