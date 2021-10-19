3e3fd9ac0f15320bb7174f8cd1f93055caa6fa2d
Takashi IIGUNI <iiguni.tks@gmail.com>
2021-10-20 04:29:00 +0900

    Add speakerdeck link
    Signed-off-by: Takashi IIGUNI <iiguni.tks@gmail.com>

--- lib/Sites.ts
+++ lib/Sites.ts
@@ -4,8 +4,9 @@ export type SitesProps = {
 }
 
 export const sitesData: SitesProps[] = [
-  { title: "GitHub",    url: "https://github.com/guni1192" },
-  { title: "Twitter",   url: "https://twitter.com/guni1192" },
-  { title: "Blog",      url: "https://guni1192.hatenablog.com" },
-  { title: "Linkedin",  url: "https://www.linkedin.com/in/takashi-iiguni" },
+  {title: 'GitHub', url: 'https://github.com/guni1192'},
+  {title: 'Twitter', url: 'https://twitter.com/guni1192'},
+  {title: 'Blog', url: 'https://guni1192.hatenablog.com'},
+  {title: 'Linkedin', url: 'https://www.linkedin.com/in/takashi-iiguni'},
+  {title: 'SpeakerDeck', url: 'https://speakerdeck.com/guni1192'},
 ]
