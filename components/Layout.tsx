668ed304a2b18250bcba217966c7caa89fb14eb8
Takashi IIGUNI <iiguni.tks@gmail.com>
2021-10-31 02:38:01 +0900

    add meta description
    Signed-off-by: Takashi IIGUNI <iiguni.tks@gmail.com>

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -15,6 +15,7 @@ const Layout = ({ children, title = 'guni1192' }: Props) => (
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="description" content="Takashi Iiguni is a Japanese software developer" />
     </Head>
     <body>
     <header>
