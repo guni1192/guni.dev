90a1168fcf437fb676a02906c22cf21da93d434f
Takashi IIGUNI <iiguni.tks@gmail.com>
2022-07-10 13:57:01 +0900

    add presentation materials
    Signed-off-by: Takashi IIGUNI <iiguni.tks@gmail.com>

--- lib/Posts.ts
+++ lib/Posts.ts
@@ -8,4 +8,8 @@ export const postsData: Posts[] = [
     title: '最所研究室 コンテナ勉強会',
     url: 'https://container-workshop.guni.dev'
   },
+  {
+    title: 'Rust no_stdで作るコンテナ',
+    url: 'https://speakerdeck.com/guni1192/rust-no-stddezuo-rukontena-6ed09e39-83e2-4218-8d96-3b7e59dd5aec'
+  },
 ]
