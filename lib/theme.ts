ba16c639f9868064b7a4085afd4edc987a0d83ea
Takashi IIGUNI <takashi.iiguni@guni.dev>
2023-07-11 06:41:41 +0900

    set default colortheme is dark
    Signed-off-by: Takashi IIGUNI <takashi.iiguni@guni.dev>

--- lib/theme.ts
+++ lib/theme.ts
@@ -2,7 +2,6 @@ import {extendTheme, ThemeConfig} from '@chakra-ui/react'
 
 const config: ThemeConfig = {
   initialColorMode: 'dark',
-  useSystemColorMode: true,
 }
 
 export const theme = extendTheme({config})
