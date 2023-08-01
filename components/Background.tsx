95dd2e641382218db17c846f4f5cebeb5e8662e9
Takashi IIGUNI <takashi.iiguni@guni.dev>
2023-08-01 12:48:48 +0900

    fix background date
    Signed-off-by: Takashi IIGUNI <takashi.iiguni@guni.dev>

--- components/Background.tsx
+++ components/Background.tsx
@@ -22,6 +22,13 @@ function getMonth(date: Date): string {
   return zeroPaddingMonth;
 }
 
+function dateToString(date: Date | null): string {
+  if (date === null) {
+    return '';
+  }
+  return date.getFullYear() + '/' + getMonth(date)
+}
+
 const Background = ({ title, events }: Props ) => (
   <div>
     <Heading as="h2" size="xl">{title}</Heading>
@@ -30,10 +37,10 @@ const Background = ({ title, events }: Props ) => (
       align='stretch'
       >
         {
-          events.sort((a: Event, b: Event) => (a.endDate < b.endDate)? 1 : -1).map((event, index) =>
+          events.sort((a: Event, b: Event) => (a.startDate < b.startDate)? 1 : -1).map((event, index) =>
           <Box key={index}>
             <Heading as="h3" size="md">{event.name}</Heading>
-            <Text fontSize="xs">{event.startDate.getFullYear()}-{getMonth(event.startDate)}  - {event.endDate.getFullYear()}-{getMonth(event.endDate)} </Text>
+            <Text fontSize="xs">{dateToString(event.startDate)} - {dateToString(event.endDate)}</Text>
             <Text>{event.body}</Text>
             </Box>
             )
