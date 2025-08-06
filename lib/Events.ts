9c71b71119f2b49500b88e76866e3059dd061173
Takashi IIGUNI <takashi.iiguni@guni.dev>
2025-08-07 07:26:02 +0900

    fix typo
    Signed-off-by: Takashi IIGUNI <takashi.iiguni@guni.dev>

--- lib/Events.ts
+++ lib/Events.ts
@@ -24,6 +24,6 @@ export const backgroundData: Event[] =
       new Event('2020-04', '2022-03', 'Kagawa University, Faculty of Engineering, Division of Reliability-based Information Systems Engineering', 'Saisho Laboratory'),
       // new Event('2020-08', '2020-08', 'Cybozu, inc. Internship', 'Cloud Platform Engineer'),
       // new Event('2020-09', '2020-09', 'Yahoo Japan Corporation Internship', ''),
-      new Event('2022-04', '2023-12', 'NTT Communications', 'Software Engineer (Backend'),
+      new Event('2022-04', '2023-12', 'NTT Communications', 'Software Engineer (Backend)'),
       new Event('2024-01', null, 'Ubie, inc.', 'Software Engineer (SRE)'),
     ]
