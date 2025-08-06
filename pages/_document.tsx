d77158b7639f5acbc72dd1f29091f5529f4b6123
Takashi Iiguni <takashi.iiguni@guni.dev>
2025-08-07 08:05:28 +0900

    set favicon (#70)
    Signed-off-by: Takashi IIGUNI <takashi.iiguni@guni.dev>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,15 @@ export default function Document() {
         <meta name="theme-color" content="#0AE894" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#0AE894" />
+        <meta name="theme-color" content="#0AE894" />
       </Head>
       <body>
         <Main />
