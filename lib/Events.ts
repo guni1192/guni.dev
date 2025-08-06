4aefad29c79732f8ab8c4c027be7799e8becf387
Takashi IIGUNI <takashi.iiguni@guni.dev>
2025-08-07 07:19:51 +0900

    update background
    Signed-off-by: Takashi IIGUNI <takashi.iiguni@guni.dev>

--- lib/Events.ts
+++ lib/Events.ts
@@ -19,11 +19,11 @@ export class Event {
 export const backgroundData: Event[] =
     [
       new Event('2016-04', '2020-03', 'Kagawa University, Faculty of Engineering, Electronics and Information Engineering', 'Saisho Laboratory'),
-      new Event('2018-02', '2018-02', 'GMO Pepabo, inc. Internship', 'Frontend Engineer'),
-      new Event('2018-08', '2018-09', 'Hatena, inc. Internship', 'Site Reliabillity Engineer'),
+      // new Event('2018-02', '2018-02', 'GMO Pepabo, inc. Internship', 'Frontend Engineer'),
+      // new Event('2018-08', '2018-09', 'Hatena, inc. Internship', 'Site Reliabillity Engineer'),
       new Event('2020-04', '2022-03', 'Kagawa University, Faculty of Engineering, Division of Reliability-based Information Systems Engineering', 'Saisho Laboratory'),
-      new Event('2020-08', '2020-08', 'Cybozu, inc. Internship', 'Cloud Platform Engineer'),
-      new Event('2020-09', '2020-09', 'Yahoo Japan Corporation Internship', ''),
-      new Event('2022-04', '2023-12', 'NTT Communications', 'Software Engineer'),
+      // new Event('2020-08', '2020-08', 'Cybozu, inc. Internship', 'Cloud Platform Engineer'),
+      // new Event('2020-09', '2020-09', 'Yahoo Japan Corporation Internship', ''),
+      new Event('2022-04', '2023-12', 'NTT Communications', 'Software Engineer (Backend'),
       new Event('2024-01', null, 'Ubie, inc.', 'Software Engineer (SRE)'),
     ]
